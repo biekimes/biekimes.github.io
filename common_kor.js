b467920707233f7e5f2017da44e054dab43d97fb
biekimes <biekimes@gmail.com>
2021-06-11 09:51:47 +0900

    hice overlay 11 de junio

--- common_kor.js
+++ common_kor.js
@@ -1,8 +1,5 @@
-
-
-
 function nameCheck() {
-    var name = $("#name1");
+    var name = $("#user");
     if ($(name).val().length < 1) {
         $("#nameAlert").show(100);
         $("#nameAlert").css({
@@ -10,21 +7,14 @@ function nameCheck() {
             "font-size": "12px"
         });
         return false;
-    }
-    if (!isNan$(name).val()) {
-        alert("이름에 숫자만 쓸수 없습니다.");
-        return false;
-    }
-    if ($(id).val().indexOf(" ") != -1) {
-        alert("이름에는 공백을 쓸수 없습니다.");
-        return false;
+    } else if ($(name).val().length >= 1) {
+        $("#nameAlert").hide(100);
     }
     return true;
 }
 
 function emailCheck() {
     var email = $("#email");
-
     if ($(email).val().length < 1) {
         $("#emailHelp").hide();
         $("#emailAlert").show(100);
@@ -32,10 +22,10 @@ function emailCheck() {
             "color": "red",
             "font-size": "12px"
         });
-
         return false;
+    } else if ($(email).val().length >= 1) {
+        $("#emailAlert").hide(100);
     }
-
     return true;
 }
 
@@ -48,6 +38,8 @@ function titleCheck() {
             "font-size": "12px"
         });
         return false;
+    } else if ($(title).val().length >= 1) {
+        $("#titleAlert").hide(100);
     }
     return true;
 }
@@ -61,6 +53,84 @@ function msgCheck() {
             "font-size": "12px"
         });
         return false;
+    } else if ($(msg).val().length >= 1) {
+        $("#msgAlert").hide(100);
     }
     return true;
 }
+
+$(function () {
+
+    $(".dropdown-menu").hide();
+    $(".dropdown").mouseenter(function () {
+        $(this).children(".dropdown-menu").stop().slideDown(300);
+    });
+    $(".dropdown").mouseleave(function () {
+        $(this).children(".dropdown-menu").stop().slideUp(300);
+    });
+
+    var max = 8;
+    var width = 220;
+    var index = 0;
+    var sw = 1;
+    var flag = setInterval(function () {
+        index += sw;
+        $(".gallery").animate(
+            { "margin-left": (width * -1) * index + "px" }, 2000);
+        if (index == max - 1 || index == 0)
+            sw = -sw;//
+    }, 4000);
+
+    $(".foster_pic").mouseenter(function () {
+        $(this).children(".foster_info").fadeIn(200);
+    });
+    $(".foster_pic").mouseleave(function () {
+        $(this).children(".foster_info").fadeOut(200);
+    });
+
+    $(".hide").hide();
+    $(".show_more").click(function () {
+        $(".hide:hidden").slice(0, 1).slideDown(300);
+        if ($(".hide:hidden").length === 0) {
+            $(".show_more").attr("disabled", "true");
+            $(this).text("더 없음");
+        }
+    });
+
+    $(window).scroll(function () {
+        if ($(window).scrollTop() > 800) {
+            $(".top_btn").addClass('show')
+        } else {
+            $(".top_btn").removeClass("show");
+        }
+    });
+
+    $(".top_btn").on('click', function (e) {
+        e.preventDefault();
+        $('html').animate({ scrollTop: 0 }, 100);
+    });
+
+    $(".contact_form").hide();
+    $("#contact").click(function () {
+        $(".contact_form").fadeIn(300);
+        $("#overlay").css("display", "block")
+    });
+
+    $("#nameAlert").hide();
+    $("#emailAlert").hide();
+    $("#titleAlert").hide();
+    $("#msgAlert").hide();
+    $("form").submit(function (e) {
+        if (!nameCheck()) return false;
+        if (!emailCheck()) return false;
+        if (!titleCheck()) return false;
+        if (!msgCheck()) return false;
+    });
+
+    $(".contact_form button:nth-of-type(2)").click(function () {
+        $(".contact_form").hide(100);
+        $("#overlay").css("display", "none")
+        return false;
+    });
+
+})
